1ad776ba25d54cdba3ab16bef759ac370bfb8fa4
22091432 <22091432@studentmail.ul.ie>
2023-04-25 23:39:56 +0100

    Initial Commit

---
+++ InteractivePrototypeAssignment/Assets/Scripts/Projectile2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Projectile2 : MonoBehaviour
+{
+    public int CurrentSpeed;
+    public Transform x, y, z;
+    public float Speed;
+    Renderer rend;
+
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+
+    void OnTriggerEnter(Collider otherObject)
+
+    {
+        if (otherObject.tag == "Respawn")
+        {
+            StartCoroutine(ChangeScene());
+        }
+
+    }
+
+    IEnumerator ChangeScene()
+
+    {
+        rend.enabled = false;
+        yield return new WaitForSeconds(3f);
+        rend.enabled = true;
+        SceneManager.LoadScene(3);
+    }
+}
---
+++ InteractivePrototypeAssignment/Assets/Scripts/Projectile4.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Projectile4 : MonoBehaviour
+{
+
+    public int CurrentSpeed;
+    public Transform x, y, z;
+    public float Speed;
+    Renderer rend;
+
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+
+    void OnTriggerEnter(Collider otherObject)
+
+    {
+        if (otherObject.tag == "Respawn")
+        {
+            StartCoroutine(ChangeScene());
+        }
+
+    }
+
+    IEnumerator ChangeScene()
+
+    {
+        rend.enabled = false;
+        yield return new WaitForSeconds(3f);
+        rend.enabled = true;
+        SceneManager.LoadScene(5);
+    }
+
+}
